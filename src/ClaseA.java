01f1b0e8ef25e8c0c7b99b5b6b592b02703a1727
Manu <nole_89@hotmail.com>
2013-10-18 17:18:03 +0200

    Get & Set A

--- src/ClaseA.java
+++ src/ClaseA.java
@@ -4,5 +4,13 @@ public class ClaseA {
 
 		public ClaseA(String a) {
 			this.a = a;
-		}		
+		}
+
+		public String getA() {
+			return a;
+		}
+
+		public void setA(String a) {
+			this.a = a;
+		}	
 }
